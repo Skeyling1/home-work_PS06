b463631b42f713825b25b33957cd7057f97b79d8
Skeyling1 <skeyling1@gmail.com>
2024-07-15 14:36:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,16 @@
 # но теперь ещё и сохраните информацию в csv файл.
 # # К дз прикрепляем ссылку на репозиторий с файлом csv и кодом.
 
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox()
-browser.get('https://www.divan.ru/syktyvkar/category/dekor')
+browser.get('https://www.divan.ru/syktyvkar/category/tovary_dla_doma')
 
+ggg = browser.find_element(By.TAG_NAME, "a")
 
+print(ggg.text)
+
+with open("goods.csv", 'w') as file:
+    
